51c3db73b29cce25fd7a690ef28f13b9ca5cfda3
boistean26 <121050685+boistean26@users.noreply.github.com>
2023-01-13 10:21:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function responsiveMenu() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+function hideMenu() {
+    var x = document.getElementById("myNav");
+    if (x.className !== "nav") {
+        x.className = "nav";
+    }
+}
